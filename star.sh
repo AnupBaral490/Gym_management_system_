0f6f820d1621aaa4a6e8b39ab1fa1b4fa2660985
AnupBaral490 <baralanup832@gmail.com>
2026-01-23 11:52:46 +0545

    Make star.sh executable

--- star.sh
+++ star.sh
@@ -1,3 +1,3 @@
+
 #!/bin/bash
-# Start Django with Gunicorn on Render
-gunicorn Gym_management_system.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn gym_management_system.wsgi:application --bind 0.0.0.0:$PORT
